e26ea3be98b049d6424a450023d217923a7bdcd5
Rudranshpardeshi09 <rudranshpardeshi12@gmail.com>
2026-01-31 18:49:56 +0530

    fixed LangChain deprecation by using langchain-huggingface embeddings

--- app/vectorstore/faiss_store.py
+++ app/vectorstore/faiss_store.py
@@ -1,6 +1,7 @@
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from app.core.config import settings
+
 
 
 def get_embeddings():
